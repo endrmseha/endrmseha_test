e55c49b90cc7a8fdaa6bebf82da3682b28a56639
endrmseha <endrmseha@naver.com>
2023-07-31 12:15:56 +0900

    init commit

---
+++ src/main/java/com/sparta/endrmseha_test/EndrmsehaTestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.endrmseha_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndrmsehaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndrmsehaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/endrmseha_test/EndrmsehaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.endrmseha_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndrmsehaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
